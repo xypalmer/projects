962efee6c129d02eb31695d1f9ab2c32a7d35d87
xypalmer <xypalmer@icloud.com>
2014-07-17 15:19:37 -0700

    testing stuff

---
+++ Tic Tac Toe/Random Color Boxes/angular.js
@@ -0,0 +1,3 @@
+var colorArray = ["#f5e6c4", "#ff7771", "#835c72", "#cb6e71", "#62bdb0", "#91dec3", "#6497a1", "#f1d365", "#ec4a2b", "#5b1c35" ];
+
+var ranColor = colorArray[Math.floor(Math.random() * colorArray.length)];
