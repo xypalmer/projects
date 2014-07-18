3d39a989a59bf745b47e827f22828377c35ea93f
xypalmer <xypalmer@icloud.com>
2014-07-18 09:51:37 -0700

    working except for X's and O's

--- Tic Tac Toe/Angular Version/js/angular.js
+++ Tic Tac Toe/Angular Version/js/angular.js
@@ -11,7 +11,6 @@ tttApp.controller('tttController', function ($scope) {
   $scope.row1 = [0, 0, 0];
   $scope.row2 = [0, 0, 0];
   $scope.row3 = [0, 0, 0];
-  $scope.insidebox = "";
   $scope.grid =[$scope.row1, $scope.row2, $scope.row3];
   $scope.test = "Hello";
 
@@ -19,31 +18,67 @@ tttApp.controller('tttController', function ($scope) {
     // background-color: ranColor;
     // }
 
-  $scope.input = function(r, c) {
+  $scope.input = function(row, column) {
     if (tcounter % 2 == 1 ) {
-        if ($scope.grid[r][c] == 0 || $scope.grid[r][c] == "p1b") {
-            $scope.grid[r][c] = 1;
-            $scope.insidebox = "X";
+        if ($scope.grid[row][column] == 0 || $scope.grid[row][column] == "p1b") {
+            $scope.grid[row][column] = 1;
+            // $scope.insidebox[r][c] = "X";
             tcounter++;
         }
-        else if ($scope.grid[r][c] == "p2b" ) {
-            $scope.grid[r][c] = 0;
+        else if ($scope.grid[row][column] == "p2b" ) {
+            $scope.grid[row][column] = 0;
             tcounter++;
         }
     }
     else if (tcounter % 2 == 0 ) {
-        if ($scope.grid[r][c] == 0 || $scope.grid[r][c] == "p2b") {
-            $scope.grid[r][c] = -1; 
-            $scope.insidebox = "O";
+        if ($scope.grid[row][column] == 0 || $scope.grid[row][column] == "p2b") {
+            $scope.grid[row][column] = -1; 
+            // $scope.insidebox[r][c] = "O";
             tcounter++;
         }
-        else if ($scope.grid[r][c] == "p1b") {
-            $scope.grid[r][c] = 0;
+        else if ($scope.grid[row][column] == "p1b") {
+            $scope.grid[row][column] = 0;
             tcounter++;
         }
     }
-  }
+       for (r = 0; r < $scope.grid.length; r++ ) {
+      var rcounter = 0;
+      for( c = 0; c < $scope.row1.length; c++) {
+          rcounter += $scope.grid[r][c];
+      }
+      if (rcounter == 3) {
+          alert("Player1 Wins!");
+      }
+      else if (rcounter == -3) {
+          alert("Player2 Wins!");
+      }  
 
+      var ccounter = 0;
+      for ( crow = 0; crow < $scope.row1.length; crow++) {
+          ccounter += $scope.grid[crow][r];
+      }
+      if (ccounter == 3) {
+          alert("Player1 Wins!");
+      }
+      else if (ccounter == -3) {
+          alert("Player2 Wins!");
+      }
+      diagcounter1 += $scope.grid[r][r];
+      if (diagcounter1 == 3) {
+          alert("Player1 Wins");
+      }
+      else if (diagcounter1  == -3) {
+          alert("Player2 Wis");
+      }
+      diagcounter2 += $scope.grid[r][2 - r];
+      if (diagcounter2  == 3) {
+          alert("Player1 Wins");
+      }
+      else if (diagcounter2  == 3) {
+          alert("Player2 Wins");
+      }
+    }
+  }
 
 });
 
